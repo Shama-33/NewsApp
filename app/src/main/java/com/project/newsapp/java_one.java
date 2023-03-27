5169d07183f0194d330c8581eae0d86d6df97287
unknown <shamaoishee@gmail.com>
2023-03-27 23:24:12 +0600

    java_one committed

---
+++ app/src/main/java/com/project/newsapp/java_one.java
@@ -0,0 +1,27 @@
+package com.project.newsapp;
+
+public class java_one {
+    int num;
+    String word;
+
+    public java_one(int num, String word) {
+        this.num = num;
+        this.word = word;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
